11591a79389c76fe7668916af9c6de55dbbd4902
ste0409 <104455345+ste0409@users.noreply.github.com>
2023-01-03 11:17:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,19 @@
 import streamlit
 import snowflake.connector
+import pandas
 
 streamlit.title('Athleisure Catalog')
 
+# connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select current_user(), current_account(), current_region()")
 
-my_data_row = my_cur.fetchone()
+# run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
 
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#put the data into a dataframe
+df = pandas.dataframe(my_catalog)
+
+#temp write the dataframe to the page so I can see what I am working with
+streamlit.write(df)
