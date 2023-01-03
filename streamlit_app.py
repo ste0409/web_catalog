cdcac4e369e83e6b243304e5adfdd058c8be5139
ste0409 <104455345+ste0409@users.noreply.github.com>
2023-01-03 11:11:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@ import streamlit
 import snowflake.connector
 
 streamlit.title('Athleisure Catalog')
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursur()
+my_cur.execute("select current_user(), current_account(), current_region()")
+
+my_data_row = my_cur.fetchone()
+
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
